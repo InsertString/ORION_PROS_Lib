fb6b9d436237979e85d1d634300f53a10f93b01b
Joshua Choi <joshuacanadian@gmail.com>
2022-03-25 05:11:55 -0700

    added src for lib and initial lib

---
+++ include/main.h
@@ -0,0 +1,32 @@
+
+#ifndef _PROS_MAIN_H_
+#define _PROS_MAIN_H_
+
+#define PROS_USE_SIMPLE_NAMES
+#define PROS_USE_LITERALS
+
+#include "api.h"
+#include "ORION/ORION_includes.hpp"
+
+// Add robot spesific includes here
+
+using namespace pros;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void autonomous(void);
+void initialize(void);
+void disabled(void);
+void competition_initialize(void);
+void opcontrol(void);
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+#endif
+
+// add robot spesific externs for global objects here
+
+#endif  // _PROS_MAIN_H_
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+
+// Robot Spesific Objects
+
+void initialize() {
+	
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+
+	while (true) {
+		delay(10);
+	}
+}
